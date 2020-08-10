8116a1ece05e050d2182431d2c5a72be7fd97266
Your Name <andyd19@naver.com>
2020-08-10 18:18:26 +0900

    Updates

---
+++ precache-manifest.e8ee6a27421283cef4d1b3f6a3ed1882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84dbbe615405613622ce9a53f612406b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "8dab6dd492b11306c6aa",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dab6dd492b11306c6aa",
+    "url": "/movie_app_2020/static/js/main.b63bb063.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
